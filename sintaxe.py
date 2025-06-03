3fb26b757147922f5b3c43e8779b4202673fd8b4
szcleiton <souzacleiton54@gmail.com>
2025-06-03 09:21:05 -0300

    chore: aprimorate my habilities

---
+++ sintaxe.py
@@ -0,0 +1,8 @@
+# esse é meu primeiro código em Python
+
+"""
+    Sintaxe básica do Python
+    - Comentários são feitos com # ou com aspas triplas
+"""
+
+nome = "Cleiton" + "Souza"
---
+++ variaveis.py
@@ -0,0 +1,4 @@
+nome_completo = "Cleiton Souza"
+idade = 23
+
+nomeCompleto = "Cleiton Souza"
