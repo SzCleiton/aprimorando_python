35a8633346ef7badff0b08f14536a2c330b03a45
szcleiton <souzacleiton54@gmail.com>
2025-06-03 09:56:34 -0300

    chore: I added some of the parts of how I will use the number variables, to explain this simple project that uses the terminal to work

--- variable_types/numbers.py
+++ variable_types/numbers.py
@@ -8,3 +8,35 @@ print("Real:", number_real)
 
 # type()
 print("Type of var int:", type(number_int))
+
+#some +
+some = 1 + 1
+print("1 + 1 :", some)
+
+#sub -
+sub = 10 - 5
+print("10 - 5 :", sub)
+
+# multiplication *
+mult = 2 * 3
+print("2 * 3 :", mult)
+
+# div /
+div = 5 / 2
+print("5 / 2 :", div)
+print("Type of var div:", type(div))
+
+# int()
+print("Value in int: ", int(div))  # Convert to int
+
+# float()
+print("Value in float: ", float(some))  # Convert to float
+
+# module %
+mod = 5 % 2
+print("5 % 2 :", mod)
+
+# div //
+div = 5 // 2
+print("5 // 2 :", div)
+print("Type of var div:", type(div))
