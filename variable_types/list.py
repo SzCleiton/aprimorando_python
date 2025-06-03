4ba4a41e03ff0eb7d649d93ab15eb66b9b789cc7
szcleiton <souzacleiton54@gmail.com>
2025-06-03 13:38:28 -0300

    chore: add lists

---
+++ variable_types/list.py
@@ -0,0 +1,13 @@
+# declaration
+my_list = [1, 2, 3, 4, 5, "rocketseat", True, False]
+
+print("my list the example", my_list)
+
+# exi the list
+my_list[0] = "python"
+print("my list the example", my_list)
+print("my list the example[0]", my_list[0])
+print("my list the example[5]", my_list[5])
+print(my_list[1:7])
+print(my_list[:6])
+print("my_list[2:]", my_list[2:])
